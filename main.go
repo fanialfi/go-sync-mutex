641aefe17382838651c4fb2b942b9b7d8b8628e4
fanialfi <fanialfirdaus@gmail.com>
2024-10-28 07:58:50 +0700

    implementasi sync.Mutex untuk mencegah race condition
    menempatkan sync.Mutex sebagai variabel terpisah di main

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 )
 
 type counter struct {
-	mu  sync.Mutex
 	val int
 }
 
 func (c *counter) Add(int) {
-	c.mu.Lock()
 	c.val++
-	c.mu.Unlock()
 }
 
 func (c *counter) Value() int {
@@ -27,6 +24,7 @@ func main() {
 
 	var (
 		wg    sync.WaitGroup
+		mu    sync.Mutex
 		meter counter
 	)
 	fmt.Printf("size of counter struct : %d\n", unsafe.Sizeof(meter))
@@ -36,7 +34,9 @@ func main() {
 
 		go func() {
 			for j := 0; j < 1000; j++ {
+				mu.Lock()
 				meter.Add(1)
+				mu.Unlock()
 			}
 
 			wg.Done()
